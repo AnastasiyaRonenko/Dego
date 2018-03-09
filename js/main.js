12c4cc91105bd18cace623d61bdd0d81c063249f
AnastasiyaRonenko <ronenko94@gmail.com>
2018-03-09 20:26:25 +0200

    fourth commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+	 $('.j-secondSlider').slick({
+  		infinite: true,
+  		speed: 500,
+  		fade: true,
+  		cssEase: 'linear'
+	 });
+});
+
+$(function(){
+	 $('.j-firstSlider').slick({
+  		infinite: true,
+  		speed: 1000,
+  		fade: true,
+  		cssEase: 'linear',
+  		arrows: false,
+  		autoplay: true
+	 });
+});
